abfd00987efb6dafcb664e005a3b040e61875634
Cheer <cripaktcheer@gmail.com>
2020-05-01 21:47:57 +0800

    sign in/out function done'

--- app/assets/scripts/components/HeaderLoggedOut.js
+++ app/assets/scripts/components/HeaderLoggedOut.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import Axios from "axios"
 
-const HeaderLoggedOut = () => {
+function HeaderLoggedOut(props) {
   const [username, setUsername] = useState()
   const [password, setPassword] = useState()
 
@@ -10,12 +10,12 @@ const HeaderLoggedOut = () => {
     try {
       const response = await Axios.post("http://localhost:8080/login", { username, password })
       if (response.data) {
-        console.log(response.data)
+        props.setLoggedIn(true)
       } else {
-        console.log("incorrect username and password")
+        console.log("incorrect username / password")
       }
     } catch (e) {
-      console.log("There was a problem")
+      console.log(e)
     }
   }
 
