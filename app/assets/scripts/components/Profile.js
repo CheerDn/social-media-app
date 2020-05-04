590b640fe81d58702bea1d7af986333a1a6813c5
Cheer <cripaktcheer@gmail.com>
2020-05-04 13:41:17 +0800

    profile screen done

---
+++ app/assets/scripts/components/Profile.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useContext, useState } from "react"
+import Page from "./Page"
+import { useParams } from "react-router-dom"
+import Axios from "axios"
+import StateContext from "../StateContext"
+import PfofilePosts from "./ProfilePosts"
+import ProfilePosts from "./ProfilePosts"
+
+function Profile() {
+  const { username } = useParams()
+  const appState = useContext(StateContext)
+  const [profileData, setProfileData] = useState({
+    profileUsername: "...",
+    profileAvatar: "...",
+    isFollowing: false,
+    counts: { postCount: 17, followerCount: 10, followingCount: 3 }
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await Axios.post(`/profile/${username}`, { token: appState.user.token })
+        setProfileData(response.data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchData
+  }, [])
+
+  return (
+    <Page title="Profile Screen">
+      <h2>
+        <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
+        <button className="btn btn-primary btn-sm ml-2">
+          Follow <i className="fas fa-user-plus"></i>
+        </button>
+      </h2>
+      <div className="profile-nav nav nav-tabs pt-2 mb-4">
+        <a href="#" className="active nav-item nav-link">
+          Post: {profileData.counts.postCount}
+        </a>
+        <a href="#" className="nav-item nav-link">
+          Follower: {profileData.counts.followerCount}
+        </a>
+        <a href="#" className="nav-item nav-link">
+          Following: {profileData.counts.followingCount}
+        </a>
+      </div>
+      <ProfilePosts />
+    </Page>
+  )
+}
+
+export default Profile
