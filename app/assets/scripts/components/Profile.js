0f77c750035750befde5b6868b21064984bd73d7
Cheer <cripaktcheer@gmail.com>
2020-05-06 18:06:53 +0800

    follower count done

--- app/assets/scripts/components/Profile.js
+++ app/assets/scripts/components/Profile.js
@@ -1,27 +1,35 @@
-import React, { useEffect, useContext, useState } from "react"
+import React, { useEffect, useContext } from "react"
 import Page from "./Page"
 import { useParams } from "react-router-dom"
 import Axios from "axios"
 import StateContext from "../StateContext"
 import ProfilePosts from "./ProfilePosts"
+import { useImmer } from "use-immer"
 
 function Profile() {
   const { username } = useParams()
   const appState = useContext(StateContext)
-  const [profileData, setProfileData] = useState({
-    profileUsername: "",
-    profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
-    isFollowing: false,
-    counts: { postCount: 17, followerCount: 10, followingCount: 3 }
+  const [state, setState] = useImmer({
+    followActionLoading: false,
+    startFollowingRequestCount: 0,
+    stopFollowingRequestCount: 0,
+    profileData: {
+      profileUsername: "...",
+      profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+      isFollowing: false,
+      counts: { postCount: 0, followerCount: 0, followingCount: 0 }
+    }
   })
 
+  //-------------------  get user profile  -------------------------
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source()
-
     async function fetchData() {
       try {
         const response = await Axios.post(`/profile/${username}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
-        setProfileData(response.data)
+        setState(draft => {
+          draft.profileData = response.data
+        })
       } catch (e) {
         console.log("There was a problem or the request might be cancelled")
       }
@@ -31,25 +39,115 @@ function Profile() {
     return () => {
       ourRequest.cancel()
     }
-  }, [])
+  }, [username])
+  //------------------- end of getting user profile --------------------
+
+  //-------------------------- follow action ---------------------------
+  useEffect(() => {
+    if (state.startFollowingRequestCount) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchData() {
+        try {
+          const response = await Axios.post(`/addFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
+            draft.profileData.isFollowing = true
+            draft.profileData.counts.followerCount++
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log("There was a problem or the request might be cancelled")
+        }
+      }
+      fetchData()
+      // cancel axios request when the component unmounted or stop being rendered
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.startFollowingRequestCount])
+  //------------------------- end of following action ---------------------
+
+  //-------------------------- unfollow action ----------------------------
+  useEffect(() => {
+    if (state.stopFollowingRequestCount) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchData() {
+        try {
+          const response = await Axios.post(`/removeFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
+            draft.profileData.isFollowing = false
+            draft.profileData.counts.followerCount--
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log("There was a problem or the request might be cancelled")
+        }
+      }
+      fetchData()
+      // cancel axios request when the component unmounted or stop being rendered
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.stopFollowingRequestCount])
+  //-------------------------- end of unfollowing action ----------------------
+
+  function startFollowing() {
+    setState(draft => {
+      draft.startFollowingRequestCount++
+    })
+  }
+
+  function stopFollowing() {
+    setState(draft => {
+      draft.stopFollowingRequestCount++
+    })
+  }
 
   return (
     <Page title="Profile Screen">
       <h2>
-        <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
-        <button className="btn btn-primary btn-sm ml-2">
-          Follow <i className="fas fa-user-plus"></i>
-        </button>
+        <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
+        {/*
+          Below is the follow button.
+          Should not display "follow" button in the following cases :
+          1. User did not log in.
+          2. User followed the profile owner.
+          3. User is profile owner him/herself.
+          4. The page is still waiting for profile data.
+        */}
+        {appState.loggedIn && !state.profileData.isFollowing && appState.user.username != state.profileData && state.profileData.profileUsername != "..." && (
+          <button onClick={startFollowing} disabled={state.followActionLoading} className="btn btn-primary btn-sm ml-2">
+            Follow <i className="fas fa-user-plus"></i>
+          </button>
+        )}
+        {/*
+          Below is the unfollow button. 
+          Same logic except "state.profileData.isFollowing" part.
+        */}
+        {appState.loggedIn && state.profileData.isFollowing && appState.user.username != state.profileData && state.profileData.profileUsername != "..." && (
+          <button onClick={stopFollowing} disabled={state.followActionLoading} className="btn btn-danger btn-sm ml-2">
+            Stop Following <i className="fas fa-user-times"></i>
+          </button>
+        )}
       </h2>
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Post: {profileData.counts.postCount}
+          Post: {state.profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Follower: {profileData.counts.followerCount}
+          Follower: {state.profileData.counts.followerCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: {profileData.counts.followingCount}
+          Following: {state.profileData.counts.followingCount}
         </a>
       </div>
       <ProfilePosts />
